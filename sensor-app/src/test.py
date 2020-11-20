eda79fdcb287b27fabb578c2fb8aa47323b401dd
akihiro0000 <64446427+akihiro0000@users.noreply.github.com>
2020-11-20 19:15:04 +0900

    Update test.py

--- sensor-app/src/test.py
+++ sensor-app/src/test.py
@@ -1,2 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+from flask import Flask, render_template, Response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return
+  
 if __name__ == '__main__':
-  print("a")
+
+    app.run(host='0.0.0.0', debug=True)
